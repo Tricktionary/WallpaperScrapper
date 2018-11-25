4722008afa904da0ffed6665c6da1206778b93a3
Tricktionary <andyaoux@hotmail.com>
2018-11-25 16:57:57 -0500

    Created parser

--- jsonParser.py
+++ jsonParser.py
@@ -1,2 +1,10 @@
 import sys
-import urllib.request as urllib2 
+import requests 
+
+URL = 'https://www.reddit.com/r/iWallpaper.json'
+
+response = requests.get(url = URL)
+
+data = response.json()
+sys.stdout = open('urlData.txt','w')
+print(data)
